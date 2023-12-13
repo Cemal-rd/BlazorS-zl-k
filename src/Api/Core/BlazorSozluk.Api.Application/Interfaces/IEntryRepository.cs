dec222959042ec365b5258de169292d48d1b9cec
Cemal Ordu <rdcemal@gmail.com>
2023-12-13 04:00:11 +0300

    GenericRepository

---
+++ src/Api/Core/BlazorSozluk.Api.Application/Interfaces/IEntryRepository.cs
@@ -0,0 +1,15 @@
+﻿using Blazorsozluk.Api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorSozluk.Api.Application.Interfaces
+{
+    public interface IEntryRepository:IGenericRepository<Entry>
+    {
+      
+
+    }
+}
---
+++ src/Api/Core/BlazorSozluk.Api.Application/Interfaces/IGenericRepository.cs
@@ -0,0 +1,16 @@
+﻿using Blazorsozluk.Api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorSozluk.Api.Application.Interfaces
+{
+    public interface IGenericRepository<TEntity> where TEntity : BaseEntity
+    {
+        Task<int> AddAsync(TEntity entity);
+        Task<int> UpdateAsync(TEntity entity);
+        Task<int> DeleteAsync(Guid id);
+    }
+}
