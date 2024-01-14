51e4b502d9c33805d03e7e36528f11214edae45c
Cemal Ordu <rdcemal@gmail.com>
2024-01-14 04:37:14 +0300

    14.01

--- src/Api/Infrastructure/Blazorsozluk.Infrastructure.Persistence/Extensions/Registration.cs
+++ src/Api/Infrastructure/Blazorsozluk.Infrastructure.Persistence/Extensions/Registration.cs
@@ -26,7 +26,10 @@ namespace Blazorsozluk.Infrastructure.Persistence.Extensions
             });
             //var seedData = new SeedData();
             //seedData.SeedAsync(configuration).GetAwaiter().GetResult();
-            services.AddScoped<IUserRepository,UserRepository>();
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<IEmailConfirmationRepository, EmailConfirmationRepository>();
+            services.AddScoped<IEntryRepository, EntryRepository>();
+            services.AddScoped<IEntryCommentRepository, EntryCommentRepository>();
             return services;
         }
     }
--- src/Api/WebApi/Blazorsozluk.Api.WebApi/Program.cs
+++ src/Api/WebApi/Blazorsozluk.Api.WebApi/Program.cs
@@ -1,4 +1,5 @@
 using Blazorsozluk.Infrastructure.Persistence.Extensions;
+using BlazorSozluk.Api.Application.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddApplicationRegistration();
 builder.Services.AddInfrastructureRegistraion(builder.Configuration);
 
 var app = builder.Build();
