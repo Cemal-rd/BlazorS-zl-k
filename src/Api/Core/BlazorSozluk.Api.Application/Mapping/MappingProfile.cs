0beb51851fcbb011775bb45d2e26be256d7e9245
Cemal Ordu <rdcemal@gmail.com>
2024-01-15 04:30:01 +0300

    login işlemleri ve jwt oluşumu

---
+++ src/Api/Core/BlazorSozluk.Api.Application/Mapping/MappingProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Blazorsozlık.common.Models.Queries;
+using Blazorsozluk.Api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorSozluk.Api.Application.Mapping
+{
+    public class MappingProfile:Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<User, LoginUserViewModel>()
+                .ReverseMap();
+        }
+    }
+}
--- src/Api/WebApi/Blazorsozluk.Api.WebApi/Program.cs
+++ src/Api/WebApi/Blazorsozluk.Api.WebApi/Program.cs
@@ -1,11 +1,18 @@
 using Blazorsozluk.Infrastructure.Persistence.Extensions;
 using BlazorSozluk.Api.Application.Extensions;
+using FluentValidation.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services
+    .AddControllers()
+    .AddJsonOptions(opt =>
+    {
+        opt.JsonSerializerOptions.PropertyNamingPolicy = null;
+    })
+    .AddFluentValidation();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
